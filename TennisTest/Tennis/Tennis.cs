c78b23e425eed12704fd7435bf62334ef4dddb4b
austinyuch <ycchang@infocloud.com.tw>
2015-08-26 18:04:36 +0800

    refactor 1.2 score equal  test case succeeded.

--- TennisTest/Tennis/Tennis.cs
+++ TennisTest/Tennis/Tennis.cs
@@ -13,14 +13,7 @@ namespace TennisScore
 
         public static String score(int playerOne, int playerTwo)
         {
-
-            if (playerOne == 1)
-                return equalDict[1] + " all";
-            if (playerOne == 2)
-                return equalDict[2] + " all";
-            if (playerOne == 3)          
-                return equalDict[3] + " all";
-            return "Love all";
+            return equalDict[playerOne] + " all"; 
 
         }
     }
