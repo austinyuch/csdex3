ce6c8f0daae36bbaca027539df71a9525fb846f4
austinyuch <ycchang@infocloud.com.tw>
2015-08-26 15:10:33 +0800

    1st test case suuccessed.

---
+++ TennisTest/Tennis/Tennis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using TennisScore;
+
+namespace TennisScore
+{
+    public class Tennis
+    {
+        public static String score(int playerOne, int playerTwo)
+        {
+            //throw new NotImplementedException();
+            return "love all";
+        }
+    }
+}
---
+++ TennisTest/TennisTest/TennisTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using TennisScore;
+
+namespace TennisScore
+{
+    [TestFixture()]
+    public class TennisTest
+    {
+        [Test]
+        public void score()
+        {
+            Assert.AreEqual("love all", Tennis.score(0,0));
+        }
+    }
+}
