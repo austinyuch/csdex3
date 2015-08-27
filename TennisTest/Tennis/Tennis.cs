eb72053908b35bf5745f2c5da38866278bb590a5
austinyuch <ycchang@infocloud.com.tw>
2015-08-27 13:00:30 +0800

    after extract winnable method.

--- TennisTest/Tennis/Tennis.cs
+++ TennisTest/Tennis/Tennis.cs
@@ -10,20 +10,28 @@ namespace TennisScore
     public class Tennis
     {
         private static String[] dictionary = new String[] { "Love", "Fifteen", "Thirty","Forty"};
+        private static string winnable(int playerOne, int playerTwo)
+        {
+            if (playerOne - playerTwo == 1)
+                return "Player one" + " advantage";
+            if (playerOne == playerTwo)
+                return "Deuce";
+            return "Player one" + " win";
+            //throw new NotImplementedException();
+        }
 
         public static String score(int playerOne, int playerTwo)
         {   
             if (playerOne >= 4 || playerTwo >= 4)
             {
-                if (playerOne - playerTwo == 1)
-                    return "Player one" + " advantage";
-                if (playerOne == playerTwo)
-                    return "Deuce";
-                return "Player one" + " win";
+                return winnable(playerOne, playerTwo);
+              
             }
             if (playerOne != playerTwo)
                 return dictionary[playerOne] + " Love";
             return dictionary[playerOne] + " all";
         }
+
+       
     }
 }
